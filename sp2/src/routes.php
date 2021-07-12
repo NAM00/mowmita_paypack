99af3237818205df05d547bd446b39ad702f69ad
NAM00 <66006043+NAM00@users.noreply.github.com>
2021-07-12 17:54:05 +0600

    Delete sp2/src directory

--- sp2/src/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-Route::get('sp2','mowmita\sp2\Sp2Controller@index');
-Route::post('pay', 'mowmita\sp2\Sp2Controller@pay')->name('pay');
-Route::get('subtract/{a}/{b}', 'mowmita\sp2\Sp2Controller@subtract');
